f7d3b030c28b8988bdee7ddf855f31c60d077ac0
Stephen Bridges <widget@stdin.co.uk>
2010-04-25 22:59:41 +0100

    Added checking circle centre colour. Had to use the C API, C++ has no info whatsoever on pixel access.

--- seqcircles.cpp
+++ seqcircles.cpp
@@ -31,11 +31,16 @@ int main(int argc, char** argv)
         vector<Vec3f> circles;
         HoughCircles(gray, circles, CV_HOUGH_GRADIENT, 2, 40, 200, 100, 20, 28);
         cout << "Found " << circles.size() << " circles" << endl;
+        IplImage ipimg = img;
         for( size_t i = 0; i < circles.size(); i++ )
         {
             Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
             int radius = cvRound(circles[i][2]);
-            cout << "Centre (" << circles[i][0] << ", " << circles[i][1] << ") radius " << circles[i][2] << endl;
+            cout << "Centre (" << circles[i][0] << ", " << circles[i][1] << ") radius " << circles[i][2];
+            cout << ", colour (";
+            CvScalar s;
+            s = cvGet2D(&ipimg, circles[i][1], circles[i][0]);
+            cout << s.val[0] <<  ", " << s.val[1] << ", " << s.val[2] << ")" <<endl;
             // draw the circle center
             circle( img, center, 3, Scalar(0,255,0), -1, 8, 0 );
             // draw the circle outline
